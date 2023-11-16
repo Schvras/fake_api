214c2840c01d6de22a3ae781bed484b122fa7016
schvras <henrique.jeanschvartz@gmail.com>
2023-11-16 09:27:25 -0300

    Delete

--- api/controllers/users.js
+++
@@ -1,8 +0,0 @@
-module.exports = () => {
-const usersDB = require('../data/users.json');
-const controller = {};
-
-controller.listUsers = (req, res) => res.status(200).json(usersDB);
-
-return controller;
-}
