41f87f2c5603409a9a45f037123b26b0eeb75e32
schvras <henrique.jeanschvartz@gmail.com>
2023-11-16 12:00:09 -0300

    SendSMS

---
+++ pages/api/NewContact.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    EmailId: number,
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+    console.log(req.body)
+
+    if (req.body.Emails) {
+        res.status(200).json({ 
+            EmailId: Math.random() * 10000,
+        })
+    }
+}
